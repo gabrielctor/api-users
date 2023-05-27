2c46bbfd0748329b21844bec7b154d1ccde49810
Gabriel Torquato <gabrielcvtorquato@gmail.com>
2023-05-27 19:03:36 -0300

    First commit

---
+++ src/mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Gabriel',
+    },
+    {
+        id: 2,
+        name: 'Bárbara',
+    },
+    {
+        id: 3,
+        name: 'Poki',
+    },
+    {
+        id: 4,
+        name: 'Aiko',
+    },
+];
